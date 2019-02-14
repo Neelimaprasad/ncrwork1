ab5504cfd9fb93191e8ef2ac3dc097fa3db064fb
neelima <neelimaneelu468@gmail.com>
2019-02-14 15:15:19 -0800

    complex numbers

---
+++ assignment_4/assignment_4/counting_e.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char * s[] = { "we will teach you how to ", "Move a mountain ", "Level a building ", "Erase the past ", "Make a million " };
+	int i = 0, j = 0,count=0;
+	//int len = strlen_s(s);
+	for (i = 0; i <(strlen(s)); i++)
+	{
+		for (j = 0; j <(strlen(*(s+i))); j++)
+		{
+			if ((*(*(s+i)+j) == 'e'))
+				count++;
+		}
+	}
+	printf("no of e's:%d", count);
+	getch();
+	return 0;
+
+
+}
